c9c7dacb0163015c13d52dd63ad67abe65a1f2b2
ChillStain <101631488+ChillStain@users.noreply.github.com>
2022-03-21 12:20:30 +0100

    Update Main.java

--- S1N3Ex1/src/S1N3Ex1/Main.java
+++ S1N3Ex1/src/S1N3Ex1/Main.java
@@ -3,7 +3,7 @@ package S1N3Ex1;
 public class Main {
     public static void main(String[] args) {
 
-        Base base1 = new Derivada()
+        Base base1 = new Derivada();
 
                 base1.method();
 
